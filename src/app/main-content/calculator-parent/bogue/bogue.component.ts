e6adcd27eacb4880fc8574a5eb848a962338f8af
jpphil0 <jpphil0@charter.net>
2024-04-10 12:12:44 -0500

    bogue calculator working

--- src/app/main-content/calculator-parent/bogue/bogue.component.ts
+++ src/app/main-content/calculator-parent/bogue/bogue.component.ts
@@ -13,14 +13,14 @@ import { MatButtonModule } from '@angular/material/button';
   styleUrl: './bogue.component.css'
 })
 export class BogueComponent {
-  cao: number = 64.67;
-  sio2: number = 21.03;
-  al2o3: number = 6.16;
-  fe2o3: number = 2.58;
-  so3: number = 2.03;
+  cao: number = 0;
+  sio2: number = 0;
+  al2o3: number = 0;
+  fe2o3: number = 0;
+  so3: number = 0;
   results: any;
 
-  calculateBogue(): void {
+  calculateBogue(cao: number, sio2: number, al2o3: number, fe2o3: number = 2.58, so3: number): void {
     if (this.cao <= 0 || this.sio2 <= 0 || this.al2o3 <= 0 || this.fe2o3 <= 0 || this.so3 <= 0) {
       alert("All input values must be positive non-zero numbers.");
       return;
