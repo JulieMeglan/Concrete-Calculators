45e1eb532de3ded62bf6c9cecd4274fb601dc949
jpphil0 <jpphil0@charter.net>
2024-10-21 12:36:36 -0500

    changed firebase config

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,10 +1,11 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyB7FEl3ZP_xhQa0pvYkSYUAYQMsaNm5d8o",
   authDomain: "concrete-calculators.firebaseapp.com",
-  databaseURL: "https://concrete-calculators-default-rtdb.firebaseio.com",
+  databaseURL: "https://console.firebase.google.com/u/0/project/concrete-calculators/firestore/data",
   projectId: "concrete-calculators",
   storageBucket: "concrete-calculators.appspot.com",
   messagingSenderId: "538357900190",
   appId: "1:538357900190:web:b16f3ec9c3de29a2b3ac8f",
-  measurementId: "G-XWLG3YWHL1"
+  measurementId: "G-XWLG3YWHL1",
+  databaseId: "concretecalculator-db"
   };
