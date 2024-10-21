bc33ba8ef680c735e7c914cdfc0135353f526c52
jpphil0 <jpphil0@charter.net>
2024-10-21 18:21:10 -0500

    added firestore.json

--- src/firebase.config.ts
+++ src/firebase.config.ts
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyB7FEl3ZP_xhQa0pvYkSYUAYQMsaNm5d8o",
   authDomain: "concrete-calculators.firebaseapp.com",
+  databaseURL: "https://concrete-calculators-default-rtdb.firebaseio.com",
   projectId: "concrete-calculators",
   storageBucket: "concrete-calculators.appspot.com",
   messagingSenderId: "538357900190",
   appId: "1:538357900190:web:b16f3ec9c3de29a2b3ac8f",
-  measurementId: "G-XWLG3YWHL1",
-  databaseId: "concretecalculator-db"
-  };
+  measurementId: "G-XWLG3YWHL1"
+};
